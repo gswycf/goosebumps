ce84ebd79daae5146205e5fd5649f269e304bb9b
hulala <1697782237@qq.com>
2024-03-04 09:11:34 +0000

    data processing

--- dataloader.py
+++ dataloader.py
@@ -1,29 +1,90 @@
+import os
+import cv2
+import sys
+import pdb
+import six
+import glob
+import time
 import torch
+import random
 import pandas
+import warnings
 
-def read_xlsx(path):
-    wb = pandas.read_excel(path, sheet_name = 'Sheet1')
-    head_list = wb.head()
-    data={}
-    for line in wb.head():
-        data[line]=wb[line]
-    print(len(data['rthigh']))
-    print(type(data['rthigh']))
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
+import numpy as np
+# import pyarrow as pa
+from PIL import Image
+import torch.utils.data as data
+import matplotlib.pyplot as plt
+from torch.utils.data.sampler import Sampler
+
+sys.path.append("..")
+global kernel_sizes
 
 def read_txt(path):
     data=[]
+    present = 0
     with open(path) as f:
         for line in f.readlines():
             line= line.strip("\t").strip("\n").split("\t")
             data.append(line)
+            # print(line[2:])
+            if(int(line[2])==0 and int(line[3])==0 and int(line[4])==0 and int(line[5])==0):
+                present = present+1
+
+    print("data analysis", present/(len(data))*1.0)
+
     return data
 
+class BaseFeeder(data.Dataset):
+    def __init__(self,  mode="train", input_size=5,data_path='./data.txt'):
+        self.mode = mode
+        self.input_size = input_size
+        self.data = read_txt(data_path)
+        self.split_data(self.mode)
+
+    def split_data(self, mode):
+        data_len = len(self.data)
+        if mode=="train":
+            self.data_len = int(data_len * 0.8)
+            self.data = self.data[:self.data_len]
+        elif mode=="test":
+            self.data_len=int(data_len*0.1)
+            self.data = self.data[int(data_len*0.8):int(data_len*0.9)]
+        else:
+            self.data = self.data[int(data_len*0,9):]
+            self.data_len = len(self.data)
+
+    def str2float(self, data):
+        return [float(da) for da in data]
+
+    def __getitem__(self, idx):
+        data = self.data[idx:idx+self.input_size]
+        data = np.array([self.str2float(da[1:]) for da in data])
+        datax =torch.tensor([[da] for da in data[:,0]])
+        datay = data[:,1:]
+
+        return datax, datay
+
+    def __len__(self):
+        return self.data_len - self.input_size
 
 
-def split_data(data):
-    data_len = len(data)
-
-
-if __name__ == '__main__':
-    data = read_txt('data.txt')
+if __name__ == "__main__":
+    feeder = BaseFeeder(mode='train')
+    dataloader = torch.utils.data.DataLoader(
+        dataset=feeder,
+        batch_size=1,
+        shuffle=True,
+        drop_last=True,
+        num_workers=0,
+    )
+    for data in dataloader:
+        x = data[0]
+        print(x.shape)
+        y = data[1]
+        print(y.shape)
+        y = y.int()
+        print(y)
+        break
--- model.py
+++ model.py
@@ -1,1 +1,36 @@
 import torch
+import torch.nn as nn
+
+class GooseModel(nn.Module):
+    def __init__(self, input_size=128, num_layer=2):
+        super(GooseModel, self).__init__()
+        self.input_size = input_size
+        self.num_layer=num_layer
+        self.lstm = nn.LSTM(input_size=self.input_size,
+                            hidden_size=256,
+                            num_layers=self.num_layer,
+                            batch_first=True)
+
+        self.embedding = nn.Embedding(num_embeddings=3, embedding_dim=256)
+        self.conv = nn.Conv1d(1, 256)
+        self.fc = nn.Linear(256, 2)
+
+    def shift_label(self, y):
+        b, l, dim = y.shape
+        strat_index = torch.zeros((b,1,dim))
+        y = torch.stack([strat_index, y], dim=1)
+        return y[b,:-1, :]
+
+    def forward(self, x, y):
+        y = torch.sum(y, dim=-1)
+        # x [b, 5], y [b,5,4]-> dim
+        y = self.shift_label(y)
+        y = self.embedding(y) # b 5 4 dim
+        x = self.conv(x)  # b 5 dim
+        x = self.lstm(X)
+        x = x+y
+
+
+if __name__ == '__main__':
+    data=[1,2,3]
+    print(data[:-1])
